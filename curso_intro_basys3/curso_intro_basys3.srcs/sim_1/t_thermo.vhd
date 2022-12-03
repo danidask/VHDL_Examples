1a3e5428134854e789c65d3b8f958e94105ac41d
danidask <danidask@gmail.com>
2022-12-03 19:19:09 +0000

    lab2 fixed

--- curso_intro_basys3/curso_intro_basys3.srcs/sim_1/t_thermo.vhd
+++ curso_intro_basys3/curso_intro_basys3.srcs/sim_1/t_thermo.vhd
@@ -4,19 +4,19 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 -- la entidad la dejamos vacia
-entity T_TEMPMUX is
-end T_TEMPMUX;
+entity T_THERMO is
+end T_THERMO;
 
 -- esto lo copiamos cambiando entity por component
-architecture TEST of T_TEMPMUX is
-component TEMPMUX
+architecture TEST of T_THERMO is
+component THERMO
 port (
         CURRENT_TEMP :    in bit_vector(6 downto 0);
         DESIRED_TEMP :    in bit_vector(6 downto 0);
         DISPLAY_SELECT :  in bit;
         COLD :            in bit;
         HEAT :            in bit;
-        A_C_ON :          out bit;
+        AC_ON :          out bit;
         FURNACE_ON :      out bit;
         TEMP_DISPLAY :    out bit_vector(6 downto 0)
         );
@@ -26,23 +26,23 @@ end component;
 signal CURRENT_TEMP, DESIRED_TEMP : bit_vector( 6 downto 0);
 signal DISPLAY_SELECT, COLD, HEAT : bit;
 signal TEMP_DISPLAY : bit_vector( 6 downto 0);
-signal A_C_ON, FURNACE_ON : bit;
+signal AC_ON, FURNACE_ON : bit;
 
 begin
 -- cremos component under test, y conectarmos las senales externas a las internas (con el mismo nombre)
-UUT: TEMPMUX port map(  CURRENT_TEMP => CURRENT_TEMP,
+UUT: THERMO port map(  CURRENT_TEMP => CURRENT_TEMP,
                         DESIRED_TEMP => DESIRED_TEMP,
                         DISPLAY_SELECT => DISPLAY_SELECT,
                         COLD => COLD,
                         HEAT => HEAT,
-                        A_C_ON => A_C_ON,
+                        AC_ON => AC_ON,
                         FURNACE_ON => FURNACE_ON,
                         TEMP_DISPLAY => TEMP_DISPLAY
                         );
 process
 -- aqui no hay sensitivity list, lo que hace que siempre entre y se cree un bucle infinito
 begin
-CURRENT_TEMP <= "0000000";
+CURRENT_TEMP <= "0001111";
 DESIRED_TEMP <= "1111111";
 DISPLAY_SELECT <= '0';
 COLD <= '0';
@@ -51,6 +51,10 @@ wait for 10 ns;
 DISPLAY_SELECT <= '1';
 HEAT <= '1';
 wait for 10 ns;
+DESIRED_TEMP <= "0000000";
+wait for 10 ns;
+HEAT <= '0';
+COLD <= '1';
 wait;  -- aqui hace una parada
 end process;
 end TEST;
--- curso_intro_basys3/curso_intro_basys3.srcs/sources_1/thermo.vhd
+++ curso_intro_basys3/curso_intro_basys3.srcs/sources_1/thermo.vhd
@@ -2,36 +2,40 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
-entity TEMPMUX is
+entity THERMO is
     port (
       CURRENT_TEMP :    in bit_vector(6 downto 0);
       DESIRED_TEMP :    in bit_vector(6 downto 0);
       DISPLAY_SELECT :  in bit;
       COLD :            in bit;
       HEAT :            in bit;
-      A_C_ON :          out bit;
+      AC_ON :           out bit;
       FURNACE_ON :      out bit;
       TEMP_DISPLAY :    out bit_vector(6 downto 0)
     );
-  end TEMPMUX;
+  end THERMO;
   
-  architecture Behavioral of TEMPMUX is
+  architecture Behavioral of THERMO is
   begin
-    process (DISPLAY_SELECT, CURRENT_TEMP, DESIRED_TEMP, COLD, HEAT)
+    process (DISPLAY_SELECT, CURRENT_TEMP, DESIRED_TEMP)
     begin
       if DISPLAY_SELECT = '1' then
         TEMP_DISPLAY <= CURRENT_TEMP;
       else
         TEMP_DISPLAY <= DESIRED_TEMP;
       end if;
-      A_C_ON <= '0';  -- default state if no conditions are met
+    end process;
+
+    process (DISPLAY_SELECT, CURRENT_TEMP, DESIRED_TEMP, COLD, HEAT)
+    begin
+      AC_ON <= '0';  -- default state if no conditions are met
       FURNACE_ON <= '0';
       if HEAT = '1' and CURRENT_TEMP < DESIRED_TEMP then
         FURNACE_ON <= '1';
       elsif COLD = '1' and CURRENT_TEMP > DESIRED_TEMP then
-        A_C_ON <= '1';
+        AC_ON <= '1';
       end if;
-    end process;
+    end process;    
   
   end Behavioral;
   
